bfc753a27e2a64b6696730145511a6eab30cf67d
zhangxzy <1476630148@qq.com>
2023-07-10 15:57:41 +0800

    test

---
+++ gitTest/gitTest.f90
@@ -0,0 +1,25 @@
+!  gitTest.f90 
+!
+!  FUNCTIONS:
+!  gitTest - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: gitTest
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program gitTest
+
+    implicit none
+
+    ! Variables
+
+    ! Body of gitTest
+    print *, 'Hello World'
+
+    end program gitTest
+
